6a1cc53507853e3d1ccdda3ec797cd6f14e19b9b
sixthson6 <sudais2408@gmail.com>
2025-07-04 17:27:40 +0000

    feat(security): Add Spring Security to the Config Server. Configur a security filter chain to permit all requests

--- config-server/src/main/java/com/tech/configserver/ConfigServerApplication.java
+++ config-server/src/main/java/com/tech/configserver/ConfigServerApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -12,6 +16,16 @@ public class ConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			.authorizeHttpRequests(authorize -> authorize
+				.anyRequest().permitAll()
+			)
+			.csrf(AbstractHttpConfigurer::disable);
+		return http.build();
 	}
 
 }
