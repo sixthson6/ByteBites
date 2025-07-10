279f044c5f46a04c70cc486655550bbe8280c82d
sixthson6 <sudais2408@gmail.com>
2025-07-10 19:03:11 +0000

    feat: Update pom and producer class for kafka implementation

--- order-service/src/main/java/com/tech/order_service/service/OrderEventProducer.java
+++ order-service/src/main/java/com/tech/order_service/service/OrderEventProducer.java
@@ -1,6 +1,8 @@
 package com.tech.order_service.service;
 
 import com.tech.order_service.event.OrderPlacedEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 public class OrderEventProducer {
 
     private static final String TOPIC = "order-placed-events";
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderEventProducer.class);
 
     private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
@@ -16,7 +19,11 @@ public class OrderEventProducer {
     }
 
     public void sendOrderPlacedEvent(OrderPlacedEvent event) {
-        kafkaTemplate.send(TOPIC, event.getOrderId().toString(), event);
-        System.out.println(String.format("Produced OrderPlacedEvent => %s", event));
+        try {
+            kafkaTemplate.send(TOPIC, event.getOrderId().toString(), event);
+            LOGGER.info(String.format("Produced OrderPlacedEvent => %s", event));
+        } catch (Exception e) {
+            LOGGER.error("Error sending OrderPlacedEvent to Kafka", e);
+        }
     }
 }
